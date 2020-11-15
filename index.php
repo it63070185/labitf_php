7e480bf2f3620de9167d6d352976a604599166b9
it63070185 <73686836+it63070185@users.noreply.github.com>
2020-11-15 13:25:31 +0700

    Update index.php

--- index.php
+++ index.php
@@ -39,47 +39,4 @@
                     }
                     ?>
                     </table>
-                    <?php
-                    mysqli_close($conn);
-                    ?>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </body>
-                    <?php
-                    $conn = mysqli_init();
-                    mysqli_real_connect($conn, 'apirat.mysql.database.azure.com', 'it63070185@apirat', 'UEKyfj18', 'ITFlab', 3306);
-                    if (mysqli_connect_errno($conn))
-                    {
-                        die('Failed to connect to MySQL: '.mysqli_connect_error());
-                    }
-                    $res = mysqli_query($conn, 'SELECT * FROM guestboook');
-                    ?>
-                    <table width="600" border="1">
-                    <tr>
-                        <th width="100"> <div align="center">Name</div></th>
-                        <th width="350"> <div align="center">Comment </div></th>
-                        <th width="150"> <div align="center">Action </div></th>
-                    </tr>
-                    <?php
-                    while($Result = mysqli_fetch_array($res))
-                    {
-                    ?>
-                    <tr>
-                        <td><?php echo $Result['Name'];?></div></td>
-                        <td><?php echo $Result['Comment'];?></td>
-                        <td><?php echo $Result['Action'];?><button >ลบ</button> <button>เพิ่ม</button></td>
-                    </tr>
-                    ?>
-
-                        
-                    <?php
-                    }
-                    ?>
-                    </table>
-                    <?php
-                    mysqli_close($conn);
-                    ?>
-</body>
 </html>
