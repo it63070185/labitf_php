20785af8c61dee518405ca27833f3e35588e03c7
it63070185 <73686836+it63070185@users.noreply.github.com>
2020-11-19 19:32:04 +0700

    Update delete.php

--- delete.php
+++ delete.php
@@ -1,33 +1,34 @@
-<html>
+<?php
+$conn = mysqli_connect('apirat.mysql.database.azure.com', 'it63070185@apirat', 'UEKyfj18', 'ITFlab');
+$sql = 'DELETE FROM guestbook WHERE ID = ' . $_GET['ID'] . '';
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+
 <head>
-<title>SUCCESS</title>
-<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Delete</title>
+    <link rel="stylesheet" href="css/bootstrap.min.css">
 </head>
-<body class="font-mail">
-    <div class="container">
-        <div class="row">
-            <div class="col-md-12">
-                <div class="card md-3 col-sm-12">
-                <?php
-                    mysqli_real_connect($conn, 'apirat.mysql.database.azure.com', 'it63070185@apirat', 'UEKyfj18', 'ITFlab', 3306);
-                    $sql = 'DELETE FROM guestboook WHERE ID = '.$_GET['ID'].'';
 
-                    if (mysqli_connect_errno($conn))
-                    {
-                        die('Failed to connect to MySQL: '.mysqli_connect_error());
-                    }
+<body>
+    <div class="container mt-5 text-center">
 
-                    if (mysqli_query($conn, $sql)) {
-                        echo "Successfully."; 
-                    } else {
-                        echo "Error: " . $sql . "<br>" . mysqli_error($conn);
-                    }
-                    
-                    mysqli_close($conn);
-                    ?>
-                    <a href="index.php" class="btn btn-light">Home</a>
-                </div>
-            </div>
-        </div>
-    </div> 
+        <h3>
+            <?php
+            if (mysqli_query($conn, $sql)) {
+                echo "Delete Completed";
+            } else {
+                echo "Error: " . $sql . "<br>" . mysqli_error($conn);
+            }
+            mysqli_close($conn);
+            ?>
+        </h3>
+        <a href="index.php" class="btn btn-success mt-3">OK</a>
+
+    </div>
 </body>
+
+</html>
