caac666b2d25109f4f39d788332098b5bff2ab24
it63070185 <73686836+it63070185@users.noreply.github.com>
2020-11-19 16:38:02 +0700

    Update insert.php

--- insert.php
+++ insert.php
@@ -1,26 +1,35 @@
-<?php
+<html>
+<head>
+<title>SUCCESS</title>
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
+</head>
+<body class="font-mail">
+  <div class="container">
+    <?php
 
-$conn = mysqli_init();
-mysqli_real_connect($conn, 'apirat.mysql.database.azure.com', 'it63070185@apirat', 'UEKyfj18', 'ITFlab', 3306);
-if (mysqli_connect_errno($conn))
-{
-    die('Failed to connect to MySQL: '.mysqli_connect_error());
-}
+    $conn = mysqli_init();
+    mysqli_real_connect($conn, 'apirat.mysql.database.azure.com', 'it63070185@apirat', 'UEKyfj18', 'ITFlab', 3306);
+    if (mysqli_connect_errno($conn))
+    {
+        die('Failed to connect to MySQL: '.mysqli_connect_error());
+    }
 
 
-$name = $_POST['name'];
-$comment = $_POST['comment'];
+    $name = $_POST['name'];
+    $comment = $_POST['comment'];
 
 
-$sql = "INSERT INTO guestboook (Name , Comment) VALUES ('$name', '$comment')";
+    $sql = "INSERT INTO guestboook (Name , Comment) VALUES ('$name', '$comment')";
 
 
 
-if (mysqli_query($conn, $sql)) {
-    echo "Successfully";
-  } else {
-    echo "Error: " . $sql . "<br>" . mysqli_error($conn);
-  }
+    if (mysqli_query($conn, $sql)) {
+        echo "Successfully";
+      } else {
+        echo "Error: " . $sql . "<br>" . mysqli_error($conn);
+      }
 
-mysqli_close($conn);
-?>
+    mysqli_close($conn);
+    ?>
+  </div>
+</body>
