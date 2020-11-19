a7590f7ad6806bb6ae8acca28bd390b665139d60
it63070185 <73686836+it63070185@users.noreply.github.com>
2020-11-19 19:38:51 +0700

    Update update.php

--- update.php
+++ update.php
@@ -1,36 +1,41 @@
-<html>
+<?php
+	$conn = mysqli_connect('apirat.mysql.database.azure.com', 'it63070185@apirat', 'UEKyfj18', 'ITFlab');
+
+	$name = $_POST['name'];
+	$comment = $_POST['comment'];
+	$link = $_POST['link'];
+	$id = $_POST['id'];
+
+    $sql = 'UPDATE guestboook SET Name = "'.$name.'", Comment = "'.$comment.'", Link = "'.$link.'" WHERE ID = '.$id.'';
+    
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
 <head>
-<title>SUCCESS</title>
-<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Update</title>
+    <link rel="stylesheet" href="css/bootstrap.min.css">
 </head>
-<body class="font-mail">
-    <div class="container">
-        <div class="row">
-            <div class="col-md-12">
-                <div class="card md-3 col-sm-12">
+
+<body>
+    <div class="container mt-5 text-center">
+      
+            <h3>
                 <?php
-                    $conn = mysqli_connect('apirat.mysql.database.azure.com', 'it63070185@apirat', 'UEKyfj18', 'ITFlab');
-
-                    $name = $_POST['name'];
-                    $comment = $_POST['comment'];
-                    $link = $_POST['link'];
-                    $id = $_POST['id'];
-
-                    $sql = 'UPDATE guestboook SET Name = "'.$name.'", Comment = "'.$comment.'", Link = "'.$link.'" WHERE ID = '.$id.'';
-
-
-
                     if (mysqli_query($conn, $sql)) {
-                        echo "Successfully";
+                        echo "Update Completed";
                     } else {
                         echo "Error: " . $sql . "<br>" . mysqli_error($conn);
                     }
-
                     mysqli_close($conn);
-                    ?>
-                    <a href="index.php" class="btn btn-light">Home</a>
-                </div>
-            </div>
-        </div>
-    </div> 
+                ?>
+            </h3>
+            <a href="index.php" class="btn btn-success mt-3">OK</a>
+       
+    </div>
 </body>
+
+</html>
