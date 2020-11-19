2210564d67c54783040b7d834c74350e18e111ef
it63070185 <73686836+it63070185@users.noreply.github.com>
2020-11-19 19:23:57 +0700

    Update insert.php

--- insert.php
+++ insert.php
@@ -1,42 +1,46 @@
-<html>
+<?php
+
+$conn = mysqli_init();
+mysqli_real_connect($conn, 'apirat.mysql.database.azure.com', 'it63070185@apirat', 'UEKyfj18', 'ITFlab', 3306);
+if (mysqli_connect_errno($conn)) {
+  die('Failed to connect to MySQL: ' . mysqli_connect_error());
+}
+
+
+$name = $_POST['name'];
+$comment = $_POST['comment'];
+$link = $_POST['link'];
+
+
+$sql = "INSERT INTO guestboook (Name , Comment , Link) VALUES ('$name', '$comment', '$link')";
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
 <head>
-<title>SUCCESS</title>
-<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Insert</title>
+  <link rel="stylesheet" href="css/bootstrap.min.css">
 </head>
-<body class="font-mail">
-  <div class="container">
-    <div class="row">
-      <div class="col-md-12">
-        <div class="card md-3 col-sm-12">
-          <?php
 
-          $conn = mysqli_init();
-          mysqli_real_connect($conn, 'apirat.mysql.database.azure.com', 'it63070185@apirat', 'UEKyfj18', 'ITFlab', 3306);
-          if (mysqli_connect_errno($conn))
-          {
-              die('Failed to connect to MySQL: '.mysqli_connect_error());
-          }
-
-
-          $name = $_POST['name'];
-          $comment = $_POST['comment'];
-
-
-          $sql = "INSERT INTO guestboook (Name , Comment) VALUES ('$name', '$comment')";
-
-
-
-          if (mysqli_query($conn, $sql)) {
-              echo "Successfully";
-            } else {
-              echo "Error: " . $sql . "<br>" . mysqli_error($conn);
-            }
-
-          mysqli_close($conn);
-          ?>
-          <a href="index.php" class="btn btn-light">Home</a>
-        </div>
-      </div>    
+<body>
+  <div class="container mt-5 text-center">
+      <h3>
+        <?php
+        if (mysqli_query($conn, $sql)) {
+          echo "New record created successfully";
+        } else {
+          echo "Error: " . $sql . "<br>" . mysqli_error($conn);
+        }
+        mysqli_close($conn);
+        ?>
+      </h3>
+      <a href="index.php" class="btn btn-success mt-3">OK</a>
     </div>
   </div>
 </body>
+
+</html>
