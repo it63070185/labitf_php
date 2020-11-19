3b41c8564b6594579475cf54b97705975ba6b63a
it63070185 <73686836+it63070185@users.noreply.github.com>
2020-11-19 19:21:49 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,57 +1,67 @@
 <html>
+
 <head>
-<title>ITF Lab</title>
-<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
+  <title>DATABASE</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="css/bootstrap.min.css">
 </head>
-<body class="font-mail">
-    <div class="container">
-        <div class="row">
-            <div class="col-md-12">
-                <div class="card md-3 col-sm-12">
-                    <div class="card-header bg-primary text-white d-flex justify-content-between">
-                        <h3>HOME</h4>
-                        <a href="form.php" class="btn btn-success">ADD</a>
-                    </div>
-                    <?php
-                    $conn = mysqli_init();
-                    mysqli_real_connect($conn, 'apirat.mysql.database.azure.com', 'it63070185@apirat', 'UEKyfj18', 'ITFlab', 3306);
-                    if (mysqli_connect_errno($conn))
-                    {
-                        die('Failed to connect to MySQL: '.mysqli_connect_error());
-                    }
-                    $res = mysqli_query($conn, 'SELECT * FROM guestboook');
-                    ?>
-                    <div class="card-body">
-                        <table class="table">
-                            <tr>
-                            <th width="100"> <div align="center">Name</div></th>
-                            <th width="350"> <div align="center">Comment </div></th>
-                            <th width="150"> <div align="center">Action </div></th>
-                            </tr>
-                        <?php
-                        while($Result = mysqli_fetch_array($res))
-                        {
-                        ?>
-                            <tr>
-                            <td><?php echo $Result['Name'];?></div></td>
-                            <td><?php echo $Result['Comment'];?></td>
-                            <td>
-                                <a href ="delete.php?ID=<?php echo $Result['ID'];?>" >DELETE</a>
-                                <a href ="formup.php?ID=<?php echo $Result['ID'];?>" >UPDATE</a>
-                            </td>
-                            </tr>
-                        <?php
-                        }
-                        ?>
-                        </table>
-                        <?php
-                        mysqli_close($conn);
-                        ?>
-                        </table>
-                    </div>
-                </div>
-            </div>
-        </div>            
+
+<body>
+  <?php
+  $conn = mysqli_init();
+  mysqli_real_connect($conn, 'apirat.mysql.database.azure.com', 'it63070185@apirat', 'UEKyfj18', 'ITFlab', 3306);
+  if (mysqli_connect_errno($conn)) {
+    die('Failed to connect to MySQL: ' . mysqli_connect_error());
+  }
+  $res = mysqli_query($conn, 'SELECT * FROM guestboook');
+  ?>
+  <div class="container mt-5">
+    <h1 class="text-center">DATA</h1>
+    <div class="table-responsive">
+      <table class="table table-hover table-bordered table-sm">
+        <thead class="thead-dark">
+          <tr>
+            <th width="300">
+              <div align="center">Name</div>
+            </th>
+            <th width="300">
+              <div align="center">Comment </div>
+            </th>
+            <th width="300">
+              <div align="center">Link </div>
+            </th>
+            <th width="300">
+              <div align="center">Action</div>
+            </th>
+          </tr>
+        </thead>
+        <tbody>
+          <?php
+          while ($Result = mysqli_fetch_array($res)) {
+          ?>
+            <tr>
+              <td><?php echo $Result['Name']; ?></td>
+              <td><?php echo $Result['Comment']; ?></td>
+              <td><?php echo $Result['Link']; ?></td>
+              <td>
+                <a class="btn btn-success" href="edit.php?ID=<?php echo $Result['ID']; ?>">EDIT</a>
+                <a class="btn btn-danger" href="delete.php?ID=<?php echo $Result['ID']; ?>">DELETE</a>
+              </td>
+            </tr>
+          <?php
+          }
+          ?>
+        </tbody>
     </div>
+    </table>
+  </div>
+  <a class="btn btn-warning" href="form.php">Add</a>
+  </div>
+
+  <?php
+  mysqli_close($conn);
+  ?>
 </body>
+
 </html>
