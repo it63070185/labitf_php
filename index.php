9844af5281499eb517ea18f6ad64c8bdf8930af9
it63070185 <73686836+it63070185@users.noreply.github.com>
2020-11-19 18:33:59 +0700

    Update index.php

--- index.php
+++ index.php
@@ -23,8 +23,8 @@ while($Result = mysqli_fetch_array($res))
 {
 ?>
   <tr>
-    <td><?php echo $Result['name'];?></div></td>
-    <td><?php echo $Result['comment'];?></td>
+    <td><?php echo $Result['Name'];?></div></td>
+    <td><?php echo $Result['Comment'];?></td>
     <td>
       <a href ="delete.php?ID=<?php echo $Result['ID'];?>" >Delete</a>
       <a href ="update.php?ID=<?php echo $Result['ID'];?>" >Update</a>
