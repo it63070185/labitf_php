7d53427c74bc9d1de65965df027ff2e0cacd6085
it63070185 <73686836+it63070185@users.noreply.github.com>
2020-11-19 20:06:18 +0700

    Update delete.php

--- delete.php
+++ delete.php
@@ -10,7 +10,7 @@ $sql = 'DELETE FROM guestboook WHERE ID = ' . $_GET['ID'] . '';
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Delete</title>
-    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
 </head>
 
 <body>
@@ -26,7 +26,7 @@ $sql = 'DELETE FROM guestboook WHERE ID = ' . $_GET['ID'] . '';
             mysqli_close($conn);
             ?>
         </h3>
-        <a href="index.php" class="btn btn-success mt-3">OK</a>
+        <a href="index.php" class="btn btn-primary mt-3">BACK</a>
 
     </div>
 </body>
