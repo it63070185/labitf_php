f4b454f74c8db7f5ac5b773226f510fb8de96410
it63070185 <73686836+it63070185@users.noreply.github.com>
2020-11-19 19:30:26 +0700

    Update form.php

--- form.php
+++ form.php
@@ -1,32 +1,26 @@
 <!DOCTYPE html>
 <html>
+
 <head>
-	<title>Comment Form</title>
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
+  <title>Comment Form</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="css/bootstrap.min.css">
 </head>
-<body class="font-mail">
-    <div class="container">
-        <div class="row-md-5 row-sm-12">
-            <div class="col">
-                <div class="card md-3 col-sm-12">
-                    <div class="card-header bg-primary text-white d-flex justify-content-between">
-                        <h3>Add</h3>
-                        <a href="index.php" class="btn btn-light">Back</a>
-                    </div>
-                    <div class="card-body">
-                          <form action = "insert.php" method = "post" id="CommentForm" >
-                                Name:<br>
-                                <input type="text" name = "name" id="idName" class="form-control" placeholder="Enter Name"> <br>
-                                Comment:<br>
-                                <textarea rows="2" cols="20" name = "comment" id="idComment" class="form-control" placeholder="Enter Comment"></textarea><br>
-				Link:<br>
-				<input type="text" name = "link" id="idName" class="form-control" placeholder="Enter Link"> <br>
-                                <input class="btn btn-success" type="submit" id="commentBtn">
-                            </form> 
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
+<body>
+  <div class="container">
+    <form action="insert.php" method="post" id="CommentForm">
+      <div class="form-group mt-5">
+        <label class="m-3" for="name">Name</label>
+        <input type="text" class="form-control" name="name" id="idName" placeholder="Enter Name">
+        <label class="m-3" for="comment">Comment</label>
+        <textarea rows="5" class="form-control" cols="20" name="comment" id="idComment"
+          placeholder="Enter Comment"></textarea><br>
+        <label class="m-3" for="link">Link</label>
+        <input type="text" class="form-control" name="link" id="idLink" placeholder="Enter Link">
+        <input class="btn btn-primary mt-5" type="submit" id="commentBtn">
+      </div>
+    </form>
+  </div>
 </body>
 </html>
